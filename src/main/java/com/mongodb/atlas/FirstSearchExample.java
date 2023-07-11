6ea3ded7bf6d43b7a9362929cb492949b9a5e1be
Erik Hatcher <erik.hatcher@gmail.com>
2023-07-11 12:37:31 -0400

    Exception reporting improvements

--- src/main/java/com/mongodb/atlas/FirstSearchExample.java
+++ src/main/java/com/mongodb/atlas/FirstSearchExample.java
@@ -31,58 +31,59 @@ import static com.mongodb.client.model.search.SearchOptions.searchOptions;
 import static com.mongodb.client.model.search.SearchPath.fieldPath;
 
 public class FirstSearchExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Set ATLAS_URI in your environment
         String uri = System.getenv("ATLAS_URI");
-
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
-
-            SearchOperator genresClause = compound()
-                    .must(Arrays.asList(
-                            text(fieldPath("genres"),"Drama"),
-                            text(fieldPath("genres"), "Romance")
-                    ));
-
-            Document searchQuery = new Document("phrase",
-                                        new Document("query", "keanu reeves")
-                                             .append("path", "cast"));
-
-            Bson searchStage = search(
-                    compound()
-                            .filter(List.of(genresClause))
-                            .must(List.of(SearchOperator.of(searchQuery))),
-                    searchOptions().option("scoreDetails", true)
-            );
-
-            // Create a pipeline that searches, projects, and limits the number of results returned.
-            AggregateIterable<Document> aggregationResults = collection.aggregate(Arrays.asList(
-                    searchStage,
-                    project(fields(excludeId(),
-                            include("title", "cast", "genres"),
-                            metaSearchScore("score"),
-                            meta("scoreDetails", "searchScoreDetails"))),
-                    limit(10)));
-
-            // Print out each returned result
-            //aggregation_spec.forEach(doc -> System.out.println(formatJSON(doc)));
-            aggregationResults.forEach(doc -> {
-                System.out.println(doc.get("title"));
-                System.out.println("  Cast: " + doc.get("cast"));
-                System.out.println("  Genres: " + doc.get("genres"));
-                System.out.println("  Score:" + doc.get("score"));
-                // printScoreDetails(3, doc.toBsonDocument().getDocument("scoreDetails"));
-                System.out.println("");
-            });
-
-            // Print the explain output, which shows query interpretation details
-            System.out.println("Explain:");
-            System.out.println(format(aggregationResults.explain().toBsonDocument()));
-        } catch (Exception e) {
-            System.err.println(e.getLocalizedMessage());
+        if (uri == null) {
+            throw new Exception("ATLAS_URI must be specified");
         }
 
+        MongoClient mongoClient = MongoClients.create(uri);
+        MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+        MongoCollection<Document> collection = database.getCollection("movies");
+
+        SearchOperator genresClause = compound()
+                .must(Arrays.asList(
+                        text(fieldPath("genres"),"Drama"),
+                        text(fieldPath("genres"), "Romance")
+                ));
+
+        Document searchQuery = new Document("phrase",
+                                    new Document("query", "keanu reeves")
+                                         .append("path", "cast"));
+
+        Bson searchStage = search(
+                compound()
+                        .filter(List.of(genresClause))
+                        .must(List.of(SearchOperator.of(searchQuery))),
+                searchOptions().option("scoreDetails", true)
+        );
+
+        // Create a pipeline that searches, projects, and limits the number of results returned.
+        AggregateIterable<Document> aggregationResults = collection.aggregate(Arrays.asList(
+                searchStage,
+                project(fields(excludeId(),
+                        include("title", "cast", "genres"),
+                        metaSearchScore("score"),
+                        meta("scoreDetails", "searchScoreDetails"))),
+                limit(10)));
+
+        // Print out each returned result
+        //aggregation_spec.forEach(doc -> System.out.println(formatJSON(doc)));
+        aggregationResults.forEach(doc -> {
+            System.out.println(doc.get("title"));
+            System.out.println("  Cast: " + doc.get("cast"));
+            System.out.println("  Genres: " + doc.get("genres"));
+            System.out.println("  Score:" + doc.get("score"));
+            // printScoreDetails(3, doc.toBsonDocument().getDocument("scoreDetails"));
+            System.out.println();
+        });
+
+        // Print the explain output, which shows query interpretation details
+        System.out.println("Explain:");
+        System.out.println(format(aggregationResults.explain().toBsonDocument()));
+
+        mongoClient.close();
     }
 
     private static void printScoreDetails(int indentLevel, BsonDocument scoreDetails) {
